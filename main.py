2f79c1af9333dd43fe4ef99284a697ea2357e2c2
mejomba <moojaminzadeh@gmail.com>
2023-03-07 21:19:43 +0330

    update front

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ app = APIRouter()
 app.mount("/statics", StaticFiles(directory="statics"), name="statics")
 
 
-@app.get('/')
+@app.get('/login')
 def test(request: Request):
     context = {'request': request, 'data': 'any data in python'}
-    return template.TemplateResponse('home.html', context=context)
+    return template.TemplateResponse('login.html', context=context)
 
 
 if __name__ == "__main__":
