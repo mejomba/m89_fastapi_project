384ef5c882ffb3c94708274996c76612ae446b67
mejomba <moojaminzadeh@gmail.com>
2023-03-07 17:19:11 +0330

    test route and jinja

---
+++ main.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import Request, APIRouter, FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+template = Jinja2Templates('templates')
+
+app = FastAPI()
+app.mount("/statics", StaticFiles(directory="statics"), name="statics")
+
+
+@app.get('/')
+def test(request: Request):
+    context = {'request': request, 'data': 'any data in python'}
+    return template.TemplateResponse('home.html', context=context)
+
+
+if __name__ == "__main__":
+    uvicorn.run(f'{__name__}:app', reload=True)
