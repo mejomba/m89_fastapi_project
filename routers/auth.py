735aed881f284075334d0ff3f18cc3f8f5cbf78e
mejomba <moojaminzadeh@gmail.com>
2023-03-11 03:53:03 +0330

    login with jwt

--- routers/auth.py
+++ routers/auth.py
@@ -1,6 +1,7 @@
 from fastapi import status, HTTPException, Request, Depends, APIRouter
 from sqlalchemy.orm import Session
 
+import jwt_manager
 import models.auth
 import schemas.auth
 from database_manager import get_db
@@ -21,3 +22,17 @@ def create_user(user: schemas.auth.CreateUser, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+
+@router.post('/login', response_model=schemas.auth.Token)
+def user_login(user_credentials: schemas.auth.UserLogin, db: Session = Depends(get_db)):
+    user = db.query(models.auth.User).filter(models.auth.User.email == user_credentials.email).first()
+    print(user)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid credentials")
+
+    if not utils.verify(user_credentials.password, user.password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="invalid credentials")
+
+    token = jwt_manager.create_jwt_token({"user_id": user.user_id})
+    return {"access_token": token, "token_type": "bearer"}
--- schemas/auth.py
+++ schemas/auth.py
@@ -27,3 +27,17 @@ class ResponseUser(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    user_id: Optional[str] = None
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
