c57e16c0471dceab4832b5fdf9559bbfad968176
mahsa_va877 <mahsa.vahedian.a@gmai.com>
2023-03-12 17:09:08 +0330

    api post

--- routers/post_mahsa.py
+++ routers/post_mahsa.py
@@ -12,28 +12,20 @@ router = APIRouter(tags=['posts'])
 
 @router.post('/posts', response_model=post_mahsa.ResponsePost)
 def crate_post(payload:post_mahsa.CreatePost, db: Session=Depends(get_db)):
-    payload_dict = payload.dict()
-    payload_dict.update({'status':'pending', 'user_id':1})
-    print (payload_dict)
-    new_post = posts.Post(**payload_dict)
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
 
-# @router.post('/posts', response_model=post_mahsa.User)
-# def show_post(Post:post_mahsa.Post, db: session=Depends(get_db)):
-#     db_post = db.query(posts.Post).filter(posts.Post.post_id==Post.post_id)
-#     if db_post is None:
-#        raise HTTPException(status_code=400, detail='post not found')
-#     return db_post
-#
-#
-# @router.post('/posts'/, response_model=post_mahsa.User)
-# def raed_post_user(User:post_mahsa.User, db: session=Depends(get_db)):
-#     db_post = db.query(posts.Post).filter(posts.Post.user_id==User.user_id)
-#     if db_post:
-#        return db_post
-#     raise HTTPException(status_code=400, detail='This user has no post')
+@router.post('/posts/{post_id}', response_model=post_mahsa.User)
+def show_post(Post:post_mahsa.Post, db: session=Depends(get_db)):
+    db_post = db.query(posts.Post).filter(posts.Post.post_id==Post.post_id)
+    if db_post is None:
+       raise HTTPException(status_code=400, detail='post not found')
+    return db_post
+
+
+@router.post('/posts/{user_id}', response_model=post_mahsa.User)
+def raed_post_user(current_user:User:post_mahsa.User, db: session=Depends(get_db)):
+    db_post = db.query(posts.Post).filter(auth.User.user_id==current_user.user_id).filter(posts.Post.user_id==User.user_id)
+    if db_post:
+       return db_post
+    raise HTTPException(status_code=400, detail='This user has no post')
 
 
