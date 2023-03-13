13454842b3505cced24b609a530af6093a357e3f
mejomba <moojaminzadeh@gmail.com>
2023-03-13 19:04:26 +0330

    version 0.0.1

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import uvicorn
 from fastapi import Request, FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.applications import Starlette
+from starlette.routing import Route
 from database_manager import engine
 from models import auth, posts
-import routers.auth
-import routers.post_mahsa, routers.post_amin
+# import routers.auth
+# import routers.posts
+from routers import auth, posts
 
-auth.BASE.metadata.create_all(bind=engine)
-posts.BASE.metadata.create_all(bind=engine)
+# auth.BASE.metadata.create_all(bind=engine)
+# posts.BASE.metadata.create_all(bind=engine)
 
 
-template = Jinja2Templates('templates')
+template = Jinja2Templates(directory='templates')
 
 app = FastAPI()
 app.mount("/statics", StaticFiles(directory="statics"), name="statics")
 
-app.include_router(routers.auth.router)
-app.include_router(routers.post_mahsa.router)
-app.include_router(routers.post_amin.router)
-
-
+app.include_router(posts.router)
+app.include_router(auth.router)
 
 
 @app.get('/')
@@ -33,6 +33,12 @@ def home(request: Request):
 def about(request: Request):
     context = {'request': request, 'data': 'any data in python'}
     return template.TemplateResponse('about.html', context=context)
+
+
+@app.get('/contact_us')
+def contact_us(request: Request):
+    context = {'request': request}
+    return template.TemplateResponse('contact_us.html', context=context)
 
 
 if __name__ == "__main__":
