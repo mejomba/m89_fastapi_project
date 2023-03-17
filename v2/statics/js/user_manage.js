00fdfec3eb69ccdfde5e26345dd7cb9796c07e89
mejomba <moojaminzadeh@gmail.com>
2023-03-17 23:18:16 +0330

    fix user request writer

--- v2/statics/js/user_manage.js
+++ v2/statics/js/user_manage.js
@@ -1,5 +1,4 @@
 async function writerAccess(url="", data={}) {
-    debugger
     const response = await fetch(url, {
         method: "POST",
         headers: {
@@ -27,39 +26,20 @@ async function writerAccess(url="", data={}) {
     }
 }
 
-
-// const writerRequestAccept = document.getElementById('writer-request-accept');
-// const writerRequestReject = document.getElementById('writer-request-reject');
-const writerRequestForms = document.querySelectorAll('.writer-request-form');
-writerRequestForms.forEach(function(form){
-   form.addEventListener('submit', function(e){
-      e.preventDefault()
-      console.log(e.currentTarget)
-   })
-   console.log(form.children[1])
-   console.log(form)
+const confirmRequestBtns = document.querySelectorAll('.confirm-request')
+const rejectRequestBtns = document.querySelectorAll('.reject-request')
+confirmRequestBtns.forEach(function (confirmBtn){
+    confirmBtn.addEventListener('click', function (e){
+        e.preventDefault()
+        const res = writerAccess(confirmBtn.parentElement.action, {"user_request_action": 'ok'})
+        console.log(res)
+    })
 })
-//const confirm_button = document.getElementById('confirm-request')
-//confirm_button.addEventListener('click',function(e){
-//   const res = writerAccess(e.currentTarget.parentElement.action, localStorage.getItem('access_token') ,  {
-//        user_request_action: "ok"
-//    })
-//})
-//const reject_button = document.getElementById('reject-request')
-//reject_button.addEventListener('click',function(e){
-//   const res = writerAccess(e.currentTarget.parentElement.action, localStorage.getItem('access_token') ,  {
-//        user_request_action: "reject"
-//    })
-//})
-//writerRequestForm.addEventListener('submit', function (e) {
-//    e.preventDefault()
-//    console.log(this.ok.value)
-//    console.log(this.action)
-//    console.log(e.target)
-//    console.log(e.currentTarget)
-//    console.log(e.currentTarget.parent)
-//    console.log(e.target.parent)
-//    const res = writerAccess(this.action, localStorage.getItem('access_token') ,  {
-//        user_request_action: this.ok.value
-//    })
-//});
+
+rejectRequestBtns.forEach(function (rejectBtn){
+    rejectBtn.addEventListener('click', function (e){
+        e.preventDefault()
+        const res = writerAccess(rejectBtn.parentElement.action, {"user_request_action": 'reject'})
+        console.log(res)
+    })
+})
